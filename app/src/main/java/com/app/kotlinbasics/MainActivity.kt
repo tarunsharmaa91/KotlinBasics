a0ae2b2db01a0bfb2f4acb36b31d9db4b4789a95
Tarun <tarun.sharma@digitaleconomysolutions.com>
2021-03-04 08:47:44 +0400

    Basic data type intro

--- app/src/main/java/com/app/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/app/kotlinbasics/MainActivity.kt
@@ -2,10 +2,12 @@ package com.app.kotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.kotlinbasics.basics.KotlinBasics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        KotlinBasics().basicFunctions()
     }
 }
---
+++ app/src/main/java/com/app/kotlinbasics/basics/KotlinBasics.kt
@@ -0,0 +1,8 @@
+package com.app.kotlinbasics.basics
+
+class KotlinBasics {
+    fun basicFunctions() {
+        val i = "abc"
+        println("Print value $i")
+    }
+}
