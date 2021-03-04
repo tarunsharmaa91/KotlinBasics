e4607bdd203ef39c6ca6a5bc91c650ab778169ee
Tarun <tarun.sharma@digitaleconomysolutions.com>
2021-03-04 08:56:15 +0400

    Basic data type intro

--- app/src/main/java/com/app/kotlinbasics/basics/KotlinBasics.kt
+++ app/src/main/java/com/app/kotlinbasics/basics/KotlinBasics.kt
@@ -3,6 +3,9 @@ package com.app.kotlinbasics.basics
 class KotlinBasics {
     fun basicFunctions() {
         val i = "abc"
+        val array = arrayListOf(1, 2, 3)
+        array.add(4)
         println("Print value $i")
+        println("Array values $array")
     }
 }
